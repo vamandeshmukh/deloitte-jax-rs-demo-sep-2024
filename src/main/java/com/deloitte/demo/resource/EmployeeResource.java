267b84564b0352c60cb5ca7e18cdc77e873ebcfa
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-09-26 10:04:33 +0530

    emp resource

--- src/main/java/com/deloitte/demo/resource/EmployeeResource.java
+++ src/main/java/com/deloitte/demo/resource/EmployeeResource.java
@@ -1,11 +1,16 @@
+// EmployeeResource.java 
+
 package com.deloitte.demo.resource;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.deloitte.demo.model.Employee;
 import com.deloitte.demo.service.EmployeeService;
@@ -13,7 +18,9 @@ import com.deloitte.demo.service.EmployeeService;
 @Path("/employees")
 public class EmployeeResource {
 
-	private EmployeeService empService = new EmployeeService();
+	private EmployeeService empService =  new EmployeeService();
+//	private EmployeeService empService =  EmployeeService.getInstance();
+	
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -21,12 +28,17 @@ public class EmployeeResource {
 		return empService.getAllEmployees();
 	}
 
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addEmployee(Employee employee) {
+		Employee emp = empService.addEmployee(employee);
+		return Response.status(Response.Status.CREATED).entity(emp).header("messsage", "employee added successfully!")
+				.build();
+	}
+
 //	implement these methods - 
 //	getEmployeeById
-//	@POST
-//	@Produces(MediaType.APPLICATION_JSON)
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	addEmployee
 //	updateEmployee 
 //	deleteEmployee 
 
